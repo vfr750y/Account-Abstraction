9c48685431631d03176eb2bf6a381323db30cf17
vfr750y <th91843@proton.me>
2025-11-10 12:59:10 +1300

    edit deploy script

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -11,6 +11,7 @@ contract HelperConfig is Script {
 
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
+    uint256 constant LOCAL_CHAIN_ID = 31337;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -26,4 +27,27 @@ contract HelperConfig is Script {
     function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({entryPoint: address(0)});
     }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (localNetworkConfig.entryPoint != address(0)) {
+            return localNetworkConfig;
+        }
+    }
+
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
+    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+        if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilEthConfig();
+        } else if (networkConfigs[chainId].entryPoint != address(0)) {
+            return networkConfigs[chainId];
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
+    }
+
+    // deploy a amock entry point contract
 }
+
--- script/SendPackedUserOp.s.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
-
-import {Script} from "forge-std/Script.sol";
-import {MinimalAccount} from "../src/Ethereum/MinimalAccount.sol";
-
-contract DeployMinimal is Script {}
