9200b0710163f6cabb8d17312e944ef870d2ac59
vfr750y <th91843@proton.me>
2025-11-10 12:43:20 +1300

    add HelperConfig

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    error HelperConfig__InvalidChainId();
+
+    struct NetworkConfig {
+        address entryPoint;
+    }
+
+    uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getEthSepoliaConfig();
+    }
+
+    function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789});
+    }
+
+    function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({entryPoint: address(0)});
+    }
+}
