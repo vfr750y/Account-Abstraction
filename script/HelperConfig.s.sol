a9bd43d424b55cd94e7a399f8dd86c4695cd9fc9
vfr750y <th91843@proton.me>
2025-11-10 13:03:38 +1300

    refactor helperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -7,11 +7,13 @@ contract HelperConfig is Script {
 
     struct NetworkConfig {
         address entryPoint;
+        address account;
     }
 
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
+    address constant BURNER_WALLET = 0xaba2A966cC061B17465ef2D17afF858c41ff6843;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -21,15 +23,15 @@ contract HelperConfig is Script {
     }
 
     function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789});
+        return NetworkConfig({entryPoint: 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789, account: BURNER_WALLET});
     }
 
     function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({entryPoint: address(0)});
+        return NetworkConfig({entryPoint: address(0), account: BURNER_WALLET});
     }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
-        if (localNetworkConfig.entryPoint != address(0)) {
+        if (localNetworkConfig.account != address(0)) {
             return localNetworkConfig;
         }
     }
@@ -41,7 +43,7 @@ contract HelperConfig is Script {
     function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
         if (chainId == LOCAL_CHAIN_ID) {
             return getOrCreateAnvilEthConfig();
-        } else if (networkConfigs[chainId].entryPoint != address(0)) {
+        } else if (networkConfigs[chainId].account != address(0)) {
             return networkConfigs[chainId];
         } else {
             revert HelperConfig__InvalidChainId();
