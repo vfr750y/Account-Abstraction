ab3f446efdbcaafb5d624f081335728d6394e93e
vfr750y <th91843@proton.me>
2025-11-12 09:24:56 +1300

    add import to helperconfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 import {Script} from "forge-std/Script.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
 
 contract HelperConfig is Script {
     error HelperConfig__InvalidChainId();
