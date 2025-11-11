24cf317e5ecb92745104779cfd311945fe436d7d
vfr750y <th91843@proton.me>
2025-11-12 10:02:04 +1300

    add testRecoverSignedOp

--- test/Ethereum/MinimalAccountTest.t.sol
+++ test/Ethereum/MinimalAccountTest.t.sol
@@ -59,6 +59,8 @@ contract MinimalAccountTest is Test {
         address dest = address(usdc);
         uint256 value = 0;
         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+        bytes memory executeCallData =
+            abi.encodeWithSelector(minimalAccount.execute.selector, dest, value, functionData);
 
         // Act
 
