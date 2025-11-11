9d61a7760fbe8d3448b99c8df9fdc9fcc70c3729
vfr750y <th91843@proton.me>
2025-11-11 21:50:29 +1300

    add testNonOwnerCannotExecuteCommands

--- test/Ethereum/MinimalAccountTest.t.sol
+++ test/Ethereum/MinimalAccountTest.t.sol
@@ -10,6 +10,8 @@ contract MinimalAccountTest is Test {
     HelperConfig helperConfig;
     MinimalAccount minimalAccount;
     ERC20Mock usdc;
+
+    address randomuser = makeAddr("randomUser");
     uint256 constant AMOUNT = 1e18;
 
     function setUp() public {
@@ -33,5 +35,19 @@ contract MinimalAccountTest is Test {
 
         //
         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+    }
+
+    function testNonOwnerCannotExecuteCommands() public {
+        // Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        // Act
+        vm.prank(randomuser);
+        vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
+        minimalAccount.execute(dest, value, functionData);
+        //
     }
 }
