2eadda2f3520bdaf04c8b833c6019230e944034b
vfr750y <th91843@proton.me>
2025-11-12 20:09:21 +1300

    edit sendPackedUserOp

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -25,21 +25,19 @@ contract SendPackedUserOp is Script {
         bytes32 digest = userOpHash.toEthSignedMessageHash();
 
         // 3. Sign it
-        uint8 v; 
-        bytes32 r; 
+        uint8 v;
+        bytes32 r;
         bytes32 s;
         uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-       
-        if (block.chainid == 31337){
-        (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
+
+        if (block.chainid == 31337) {
+            (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
         } else {
-          (v, r, s) = vm.sign(config.account, digest);  
-        }
-           
-        userOp.signature = abi.encodePacked(r, s, v);
-        return userOp;
+            (v, r, s) = vm.sign(config.account, digest);
         }
 
+        userOp.signature = abi.encodePacked(r, s, v);
+        return userOp;
     }
 
     function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
@@ -75,5 +73,6 @@ contract SendPackedUserOp is Script {
         bytes32 gasFees;
         bytes paymasterAndData;
         bytes signature;
-        */
+       d */
 }
+
